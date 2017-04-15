cee402624959198e55ced604b12f1622d3463aa4
Piffek <patrykpiwko123412@gmail.com>
2017-04-15 22:55:00 +0200

    psr - 4

---
+++ App/Download/Load/Download.php
@@ -0,0 +1,11 @@
+<?php 
+namespace Download\Load;
+
+class Download
+{
+	public function init()
+	{
+		echo 'cos';
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+use Download\Load\Download;
+
+require_once 'App/run.php';
+
+$download = new Download();
+$download->init();
+
+?>
