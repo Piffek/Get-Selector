26e89cd4cde6794b0d761e199d8767494d9e8dfc
Piffek <patrykpiwko123412@gmail.com>
2017-04-16 20:56:58 +0200

    method to check fiend by ID

--- src/Download/Load/Download.php
+++ src/Download/Load/Download.php
@@ -12,33 +12,41 @@ abstract class Download
 			curl_close($ch);
 		}catch(Exception $e){
 				throw new Exception("Invalid URL",0,$e);
-			}
+		}
 	}
 	
 	public function checkWithParam($url, $param)
 	{
-		$name = array();
-		$ch = curl_init($url);
-		foreach($param as $key=>$par)
-		{
-			$name[] = '?'.$key.'='.$par;
+		try {
+			$name = array();
+			$ch = curl_init($url);
+			foreach($param as $key=>$par)
+			{
+				$name[] = '?'.$key.'='.$par;
+			}
+			$implodeParam = implode('',$name);
+			curl_setopt($ch, CURLOPT_URL, $url.''.$implodeParam);
+			curl_close($ch);
+		}catch(Exception $e){
+			throw new Exception("Invalid URL",0,$e);
 		}
-		$implodeParam = implode('',$name);
-		curl_setopt($ch, CURLOPT_URL, $url.''.$implodeParam);
-		curl_close($ch);
 	}
 	
 	public function checkByID($url, $params)
 	{
-		$f = file_get_contents($url, false);
-		$data=array();
-		$dom = new DOMDocument();
-		@$dom->loadHTML($f);
-		foreach($params as $param)
-		{
-			$data = $dom->getElementById($param);
-			$html2 = $dom->saveHTML($data);
-			echo $html2;
+		try {
+			$f = file_get_contents($url, false);
+			$data=array();
+			$dom = new DOMDocument();
+			@$dom->loadHTML($f);
+			foreach($params as $param)
+			{
+				$data = $dom->getElementById($param);
+				$html2 = $dom->saveHTML($data);
+				echo $html2;
+			}
+		}catch(Exception $e){
+			throw new Exception("Invalid URL",0,$e);
 		}
 	}
 	
