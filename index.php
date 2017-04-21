5ab83b0d524a547877b022efe9a94b9226e4e86a
Piffek <patrykpiwko123412@gmail.com>
2017-04-21 18:32:53 +0200

    new method find

--- index.php
+++ index.php
@@ -6,48 +6,36 @@ require_once 'src/bootstrap.php';
 $show = new Parser();
 //echo $show->check('http://www.sklep.bielawa.pl');
 //$show->curlInitWithParam('www.sklep.bielawa.pl/show_one_product.php/',['id'=>'176']);
-/*
-$review = $show->checkByID('http://www.onet.pl/', ['boxNews' ]);
+
+/*$review = $show->checkByID('https://www.gpw.pl/', ['mainMenu', 'header' ]);
 foreach($review as $row)
 {
-	echo $row['boxNews'];
+	echo $row['mainMenu'].'<br>';
+}
+
+foreach($review as $row)
+{
+	echo $row['header'].'<br>';
 }
 */
 
-<<<<<<< HEAD
 /*
 $array = $show->checkByClass('https://www.gpw.pl/', ['iconBhWiadomosci']);
 foreach ($array as $row)
 {
 	echo $row['iconBhWiadomosci'];
 }
-=======
-
-
-$array = $show->checkByClass('http://www.onet.pl/', ['boxContent']);
-foreach ($array as $row)
-{
-	echo $row['boxContent'] . '<br>';
-}
-
->>>>>>> 2d9264c2b9a07e614cbc089815c0e8984693f0b2
 
 */
 /*
 print_r($show->checkAllId('https://www.gpw.pl/'));
 */
-<<<<<<< HEAD
 
 $array = $show->find('class',['iconBhWiadomosci'],'h3','https://www.gpw.pl/');
-=======
-/*
-$array = $show->checkAllClass('https://www.gpw.pl/');
->>>>>>> 2d9264c2b9a07e614cbc089815c0e8984693f0b2
 foreach($array as $row)
 {
 	echo $row['iconBhWiadomosci'].'<br>';
 }
-<<<<<<< HEAD
 
 
 /*$array = $show->checkAllClass('https://www.gpw.pl/');
@@ -55,8 +43,5 @@ foreach($array as $row)
 {
 	echo $row.'<br>';
 }*/
-=======
-*/
->>>>>>> 2d9264c2b9a07e614cbc089815c0e8984693f0b2
 //$show->curlInitWithParamByTag('http://www.filmweb.pl/', ['ul']);
 ?>
--- src/Download/RepoAbstract/Core.php
+++ src/Download/RepoAbstract/Core.php
@@ -31,14 +31,15 @@ abstract class Core
 			$searchPage = mb_convert_encoding($f, 'HTML-ENTITIES', "UTF-8");
 			@$domdocument->loadHTML($searchPage);
 			$dom = new DOMXPath($domdocument);
+			$data=array();
 			$review = array();
 			foreach($params as $param)
 			{
-				$results = $dom->query("//".$this->selector($selectors)."[@".$this->what($what)."='" . $param . "']");
+				$results = $dom->query("//".$this->selector($selectors)."[@".$this->what($what)."='" . $param . "']"); 
 				
 		
 				for($i=0; $results->length > $i; $i++) {
-					$review[$i][$param] = $results->item($i)->nodeValue.'<br>';
+					$review[$i][$param] = $results->item($i)->nodeValue;
 				}
 			}
 			return $review;
@@ -47,14 +48,7 @@ abstract class Core
 		}
 	}
 	
-<<<<<<< HEAD:src/Download/RepoAbstract/Core.php
 	public function checkCurl($url)
-=======
-	/*
-	 * Method to check all class with HTML file and parse to text
-	 */
-	public function checkCurlByClass($url, $params, $selectors)
->>>>>>> 2d9264c2b9a07e614cbc089815c0e8984693f0b2:src/Download/RepoAbstract/Download.php
 	{
 		try {
 			$ch =  curl_init($url);
@@ -64,20 +58,9 @@ abstract class Core
 			$searchPage = mb_convert_encoding($f, 'HTML-ENTITIES', "UTF-8");
 			@$domdocument->loadHTML($searchPage);
 			$dom = new DOMXPath($domdocument);
-<<<<<<< HEAD:src/Download/RepoAbstract/Core.php
 			$results = $dom->query('//*');
 			for($i=0; $results->length > $i; $i++) {
 				$review = $results->item($i)->nodeValue;
-=======
-			$data=array();
-			$review = array();
-			foreach($params as $param)
-			{
-				$results = $dom->query("//".$this->selector($selectors)."[@class='" . $param . "']");
-				for($i=0; $results->length > $i; $i++) {
-					$review[$i][$param] = $results->item($i)->nodeValue;
-				}
->>>>>>> 2d9264c2b9a07e614cbc089815c0e8984693f0b2:src/Download/RepoAbstract/Download.php
 			}
 			return $review;
 		}catch(Exception $e){
