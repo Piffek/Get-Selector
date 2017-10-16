d35898c039aea6db5ceda4191d78a18c2671d826
Patryk Piwko <patrykpiwko123412@gmail.com>
2017-10-16 09:23:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,28 +2,10 @@
 
 require_once 'src/bootstrap.php';
 
-
-//$show = new Src\Parser\Trigger(new Src\Parser\Parser);
-/*
-$array = $show->find('https://www.gpw.pl/', 'id');
-foreach($array as $row)
-{
-	echo $row.'<br>';
-}
-*/
-
-$rest = new Src\Rest\Trigger(new Src\Rest\FindHttp());
-
-$rest->checkMethod('http://sklep.bielawa.pl');
-
 $parser = new Src\Parser\Trigger(new Src\Parser\FindParam());
 
-$array = $parser->checkMethod('https://www.gpw.pl/', 'id');
-foreach($array as $row)
-{
-	echo $row.'<br>';
-}
-
+$array = $parser->checkMethod('https://www.gpw.pl/', 'id', ['this_id']);
+echo $array[0]['this_id'];
 
 
 ?>
